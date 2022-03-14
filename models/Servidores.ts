0380a246c82f4f391a33763b4a022db9780fdcf1
drgato <mathisebas18@gmail.com>
2022-03-13 19:07:31 -0500

    6: welcome system

--- models/Servidores.ts
+++ models/Servidores.ts
@@ -1,22 +1,48 @@
-// Ahora siganme
 import { model, Schema } from 'mongoose';
 
 export default model(
   'servidores',
   new Schema<servidores>({
-    _id: String, // la id del servidor
+    _id: String,
 
     canales: {
-      sugerencias: { type: String, default: null } // la id del canal de sugerencias
+      sugerencias: { type: String, default: null }
+    },
+
+    welcomer: {
+      channel: { type: String, default: null },
+      message: { type: String, default: 'Bienvenido {{user}} a {{server.name}}' },
+      embed: {
+        enabled: { type: Boolean, default: false },
+        color: { type: String, default: '#eb4034' },
+        thumbnail: { type: Boolean, default: false }
+      },
+      image: {
+        enabled: { type: Boolean, default: false },
+        bg: { type: String, default: 'https://static.canalapps.com/uploads/2020/08/fondos-de-pantalla-canalapps.jpg' }
+      }
     }
   })
 );
 
-// creamos un interface para vincularlo con el schema
 interface servidores {
   _id: string;
 
   canales: {
     sugerencias: string;
   };
-} // seguimos la base del schema
+
+  welcomer?: {
+    channel: string;
+    message: string;
+    embed: {
+      enabled: boolean;
+      color: `#${string}`;
+      thumbnail: boolean;
+    };
+    image: {
+      enabled: boolean;
+      bg: string;
+    };
+  };
+}
